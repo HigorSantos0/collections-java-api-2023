6c1f80d41b067d62b1cf39bdde716c4a9e6e111d
Higor Nascimento <nascimentohigor125@gmail.com>
2023-09-23 19:19:51 -0300

    Exercicios resolvidos

---
+++ src/Map/Ordenacao/AgendaEventos.java
@@ -0,0 +1,54 @@
+package Map.Ordenacao;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.*;
+
+public class AgendaEventos {
+
+    private Map<LocalDate, Evento> eventosMap;
+
+    public AgendaEventos() {
+        this.eventosMap = new HashMap<>();
+    }
+
+    public void adicionarEvento(LocalDate data, String nome, String atracao){
+        //Evento evento = new Evento(nome, atracao);
+        eventosMap.put(data, new Evento(nome, atracao));
+    }
+
+    public void exibirAgenda(){
+        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventosMap);
+        System.out.println(eventosTreeMap);
+    }
+    public void obterProximoEvento(){
+        //Set<LocalDate> dataSet = eventosMap.keySet();
+        //Collections<Evento> values = eventosMap.values();
+        //eventosMap.get();
+
+        LocalDate dataAtual = LocalDate.now();
+        LocalDate proximaData = null;
+        Evento proximoEvento = null;
+        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventosMap);//Eventos organizados em forma crescente
+        for(Map.Entry<LocalDate, Evento> entry: eventosTreeMap.entrySet()){
+            if(entry.getKey().isEqual(dataAtual) || entry.getKey().isAfter(dataAtual)){
+                proximaData = entry.getKey();
+                proximoEvento = entry.getValue();
+                System.out.println("O proximo evento: " + entry.getValue() + "Acontecera na data " + entry.getKey());
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        AgendaEventos agendaEventos = new AgendaEventos();
+
+        agendaEventos.adicionarEvento(LocalDate.of(2022, Month.JULY, 15), "Evento 1", "Atracao 1");
+        agendaEventos.adicionarEvento(LocalDate.of(2023, 10, 9), "Evento 2", "Atracao 2");
+        agendaEventos.adicionarEvento(LocalDate.of(2000, Month.JANUARY, 10), "Evento 3", "Atracao 3");
+
+        agendaEventos.exibirAgenda();
+
+        agendaEventos.obterProximoEvento();
+    }
+}
